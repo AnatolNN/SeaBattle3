e3e40f55e1f357b3c941f4832be32479c87c9d9b
AnatolNN <789456GitHub>
2018-09-19 07:18:40 +0300

    Add files of game on repository

---
+++ app/src/main/java/com/example/seabattle3/SquareButton.java
@@ -0,0 +1,11 @@
+package com.example.seabattle3;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SquareButton extends android.support.v7.widget.AppCompatImageButton {
+    public SquareButton(Context context) {
+        super(context);
+    }
+}
